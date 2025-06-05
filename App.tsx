046ff75a8101827c0d298d7e657d9cd937650b29
oliflox <83953551+oliflox@users.noreply.github.com>
2025-06-05 16:18:20 +0200

    Init + reset-project script

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+
+import AppNavigator from './navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
+} 
---
+++ styles/navigation.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const navigationStyles = StyleSheet.create({
+  tabBar: {
+    height: 60,
+    paddingBottom: 5,
+  },
+  addButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 10,
+    backgroundColor: '#BA181B',
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}); 

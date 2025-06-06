9dbc01eaa02ad37fd0d4ce016805f4d53eb08605
oliflox <83953551+oliflox@users.noreply.github.com>
2025-06-06 17:02:53 +0200

    Add navigation to ExerciseHistory in WorkoutExercises; implement handleExercisePress to navigate with exercise data.

--- screens/WorkoutExercises.tsx
+++ screens/WorkoutExercises.tsx
@@ -1,12 +1,47 @@
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import React from 'react';
 import GenericFlatList from '../components/GenericFlatList';
+import { exerciseHistory } from '../mock/exerciseHistory';
 import { exercises } from '../mock/exercises';
 
+type RootStackParamList = {
+  ExerciseHistory: {
+    exercise: {
+      id: string;
+      name: string;
+      imageUrl: string;
+      history: {
+        date: string;
+        sets: {
+          id: string;
+          repetitions: number;
+          weight: number;
+        }[];
+      }[];
+    };
+  };
+};
+
+type NavigationProp = NativeStackNavigationProp<RootStackParamList, 'ExerciseHistory'>;
+
 const WorkoutExercises: React.FC = () => {
+  const navigation = useNavigation<NavigationProp>();
+
+  const handleExercisePress = (exercise: any) => {
+    const exerciseData = exerciseHistory[exercise.id];
+    if (exerciseData) {
+      navigation.navigate('ExerciseHistory', {
+        exercise: exerciseData,
+      });
+    }
+  };
+
   return (
     <GenericFlatList
       data={exercises}
       title="Exercises"
+      onItemPress={handleExercisePress}
     />
   );
 };
